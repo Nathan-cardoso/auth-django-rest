b9150891242ac4bf8593e4a6e6e5aaa9f1144cb5
Nathan-cardoso <nathancar.linhares77@gmail.com>
2025-03-03 10:08:55 -0300

    feat: remove db test and hidden api key

--- auth_demo/settings.py
+++ auth_demo/settings.py
@@ -1,17 +1,20 @@
 from pathlib import Path
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)qr4=5#j@b%r#(vpi_eceey+j5yij!b&b$_*m2*p$h+93yr2$6'
+
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG')
 
 ALLOWED_HOSTS = []
 
